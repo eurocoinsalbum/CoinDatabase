585cfd127dec725e29c672b69d5d003160d2dabe
eurocoinsalbumhelfer <42461924+eurocoinsalbumhelfer@users.noreply.github.com>
2018-08-21 10:50:14 +0200

    Update CoinDatabase.java

--- CoinDatabase.java
+++ CoinDatabase.java
@@ -504,6 +504,8 @@ public class CoinDatabase {
         database.setRare(Countries.country_sanmarino, 2015, null, 0, 1, 2, 3, 7);
         database.setRare(Countries.country_sanmarino, 2016, null, 0, 1, 2, 3, 5, 6);
         database.setRare(Countries.country_sanmarino, 2018, null, 0, 1, 2, 3, 7);
+        database.setRare(Countries.country_irland, 2015, null, 0, 1);
+        database.setRare(Countries.country_irland, 2016, null, 0, 1, 2, 3, 4, 5, 6, 7);
     }
 
     public static void setNotExistingCoins(Database database) {
