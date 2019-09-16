0ff20480dbc06f1b480e05b0004818fc057973fc
schwaelbchen <54801838+schwaelbchen@users.noreply.github.com>
2019-09-16 20:08:49 +0200

    Update CoinDatabase.java

--- CoinDatabase.java
+++ CoinDatabase.java
@@ -594,6 +594,7 @@ public class CoinDatabase {
 
         database.setNotExists(Countries.country_lithuania, 2016, 1, 2, 3, 4, 5, 6, 7);
         database.setNotExists(Countries.country_lithuania, 2017, 2, 5, 6);
+        database.setRare(Countries.country_netherlands, 2019, null, 0, 1, 4, 5, 6, 7);
     }
 
     public static ArrayList<int[]> getCoinImages(Country country, int year, boolean fillSeries) {
